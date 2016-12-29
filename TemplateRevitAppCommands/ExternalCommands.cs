4887092246692f71e6e7ddfec9bef7ea3df6595b
antonostanin <pijon4ik1717@gmail.com>
2016-12-29 11:15:06 +0300

    PIC_Поставщик

--- TemplateRevitAppCommands/ExternalCommands.cs
+++ TemplateRevitAppCommands/ExternalCommands.cs
@@ -48,8 +48,7 @@ namespace Revit_Spec_VK
             string pathToSpecExcel = @"\\dsk2.picompany.ru\project\CAD_Settings\Revit_server\03. Project Templates\04. Водоснабжение_канализация\Спецификация\Спецификации_ВК.xlsx";
             string pathToSpecRDExcel = @"\\dsk2.picompany.ru\project\CAD_Settings\Revit_server\03. Project Templates\04. Водоснабжение_канализация\Спецификация\Спецификации_R&D.xlsx";
             string pathToUserExcel = @"\\dsk2.picompany.ru\project\CAD_Settings\Revit_server\03. Project Templates\04. Водоснабжение_канализация\Спецификация\Users_Spec_VK.xlsx";
-            string localPathExcel = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) +
-                                    "\\Спецификации_ВК.xlsx";
+            string localPathExcel = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\Спецификации_ВК.xlsx";
             string localPathUserExcel = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\ЮЗвери_ВК.xlsx";
             //    File.Copy(pathToSpecExcel, localPathExcel, true);
             File.Copy(pathToUserExcel, localPathUserExcel, true);
@@ -67,11 +66,7 @@ namespace Revit_Spec_VK
 
             var paramCatInfos = fw.GetParameterCategories(localPathExcel);
 
-            var neededCatParamInfos = allElements.Where(element => paramCatInfos.Any(x => x.CategoryName.Equals(element.Category.Name))).GroupBy(x => x.Category.Name).Select(x => new
-            {
-                CategoryName = x.Key,
-                Elements = x.ToList()
-            }).ToList();
+
 
 
 
@@ -91,98 +86,18 @@ namespace Revit_Spec_VK
                 tr.Start();
 
                 #region Заполнение Параметров по значениям
-                foreach (var categoryParam in neededCatParamInfos)
-                {
-                    foreach (var elementCategory in categoryParam.Elements)
-                    {
-                        string cat = categoryParam.CategoryName;
-                        string type = doc.GetElement(elementCategory.GetTypeId()).Name;
-                        string famName = "";
-                        FamilySymbol fs = null;
-                        if (elementCategory is FamilyInstance)
-                        {
-                            fs = (elementCategory as FamilyInstance).Symbol;
-                            famName = fs.FamilyName;
-                        }
-                        bool isSetParameter = false;
-                        foreach (var filterParam in paramCatInfos.Where(x => x.CategoryName.Equals(cat)).ToList())
-                        {
-                            if (!filterParam.TypeName.Equals("") & !type.Equals(filterParam.TypeName))
-                                if (filterParam.TypeName.Contains("*") & !(type.Contains(filterParam.TypeName.Replace("*", ""))))
-                                    continue;
-                                else if (!filterParam.TypeName.Contains("*"))
-                                    continue;
-
-                            if (!filterParam.FamilyName.Equals("") & !famName.Equals(filterParam.FamilyName))
-                                if (filterParam.FamilyName.Contains("*") & !(famName.Contains(filterParam.FamilyName.Replace("*", ""))))
-                                    continue;
-                                else if (!filterParam.FamilyName.Contains("*"))
-                                    continue;
-                            bool isValid = true;
-                            foreach (var p in filterParam.parameters)
-                            {
-                                if (p.Value.Equals("")) continue;
-                                Parameter par = elementCategory.LookupParameter(p.Name);
-                                if (par == null)
-                                    continue;
-                                string parValue = fw.GetParameterValue(par);
-                                if (p.Value.Contains("*") & parValue.Contains(p.Value.Replace("*", "")))
-                                    continue;
-                                if (parValue == p.Value)
-                                    continue;
-                                isValid = false;
-                                break;
-
-                            }
-                            if (!isValid)
-                            {
-                                //Не произошло совпадение
-                                continue;
-                            }
-                            Parameter paramToSet = elementCategory.LookupParameter(filterParam.ParameterToSet);
-                            if (paramToSet == null)
-                            {
-                                if (fs != null)
-                                    paramToSet = fs.LookupParameter(filterParam.ParameterToSet);
-                                if (paramToSet == null)
-                                {
-                                    errorMessages.Add(new ErrorMessage(string.Format("Лист Параметры. Отсутствует параметр \"{0}\"", filterParam.ParameterToSet), elementCategory.Id));
-                                    continue; //Нет заполняемого параметра
-                                }
-                            }
-                            AnalyticalModelSurface sur = null;
-
-                            isSetParameter = true;
-                            if (paramToSet.IsReadOnly)
-                            {
-                                errorMessages.Add(new ErrorMessage(string.Format("Лист Параметры. Значение параметра \"{0}\" задано формулой в семействе. ", filterParam.ParameterToSet), elementCategory.Id));
-                                continue;
-                            }
-                            paramToSet.Set(filterParam.ParameterValue);
-                            break;
-                        }
-                        if (!isSetParameter)
-                        {
-                            //Элемент не найден в Excel
-                        }
-                    }
-
-                }
+                SetParametersByValue(doc, allElements, paramCatInfos);
                 #endregion
 
                 #region Заполнение Mep_t_стенки
-                foreach (var pipe in allPypes)
-                {
-                    string type = pipe.Name;
-                    string size = pipe.LookupParameter("Размер").AsString();
-                    var p = pipeInfos.Where(x => x.Type.Equals(type) && x.Size.Equals(size)).ToList();
-                    if (p.Count == 0)
-                        continue;
-
-                    if (pipe.LookupParameter("MEP_t_стенки") == null) continue;
-                    pipe.LookupParameter("MEP_t_стенки").Set(p[0].Mep_T_Wall);
-                }
+                SetMep_t(allPypes, pipeInfos);
                 #endregion
+
+                #region Заполнение PIC_Поставщик
+                SetPIC_Creator(doc, allElements);
+                #endregion
+
+
 
 
                 foreach (var categoryNeededEl in neededElements)            //Категории элементов в модели
@@ -266,6 +181,128 @@ namespace Revit_Spec_VK
                     MessageBoxIcon.Information);
             }
             return Result.Succeeded;
+        }
+
+        private static void SetMep_t(List<Element> allPypes, List<PipeInfo> pipeInfos)
+        {
+            foreach (var pipe in allPypes)
+            {
+                string type = pipe.Name;
+                string size = pipe.LookupParameter("Размер").AsString();
+                var p = pipeInfos.Where(x => x.Type.Equals(type) && x.Size.Equals(size)).ToList();
+                if (p.Count == 0)
+                    continue;
+
+                if (pipe.LookupParameter("MEP_t_стенки") == null) continue;
+                pipe.LookupParameter("MEP_t_стенки").Set(p[0].Mep_T_Wall);
+            }
+        }
+
+        private void SetPIC_Creator(Document doc, List<Element> allElements)
+        {
+            foreach (var el in allElements)
+            {
+                FamilyInstance fi = el as FamilyInstance;
+                if (fi == null) continue;
+                Parameter par = doc.GetElement(fi.GetTypeId()).LookupParameter("PIC_Поставщик");
+                Parameter parCreator = doc.GetElement(fi.GetTypeId()).LookupParameter("Изготовитель");
+                if (par == null || parCreator == null)
+                    continue;
+                string value = GetParameterTypeValue(par, false);
+                if (value != "") continue;
+                par.Set(GetParameterTypeValue(parCreator, false));
+
+            }
+        }
+
+        private void SetParametersByValue(Document doc, List<Element> allElements, List<ParameterCategory> paramCatInfos)
+        {
+            FrameWork fw = new FrameWork();
+            var neededCatParamInfos = allElements.Where(element => paramCatInfos.Any(x => x.CategoryName.Equals(element.Category.Name))).GroupBy(x => x.Category.Name).Select(x => new
+            {
+                CategoryName = x.Key,
+                Elements = x.ToList()
+            }).ToList();
+
+            foreach (var categoryParam in neededCatParamInfos)
+            {
+                foreach (var elementCategory in categoryParam.Elements)
+                {
+                    string cat = categoryParam.CategoryName;
+                    string type = doc.GetElement(elementCategory.GetTypeId()).Name;
+                    string famName = "";
+                    FamilySymbol fs = null;
+                    if (elementCategory is FamilyInstance)
+                    {
+                        fs = (elementCategory as FamilyInstance).Symbol;
+                        famName = fs.FamilyName;
+                    }
+                    bool isSetParameter = false;
+                    foreach (var filterParam in paramCatInfos.Where(x => x.CategoryName.Equals(cat)).ToList())
+                    {
+                        if (!filterParam.TypeName.Equals("") & !type.Equals(filterParam.TypeName))
+                            if (filterParam.TypeName.Contains("*") & !(type.Contains(filterParam.TypeName.Replace("*", ""))))
+                                continue;
+                            else if (!filterParam.TypeName.Contains("*"))
+                                continue;
+
+                        if (!filterParam.FamilyName.Equals("") & !famName.Equals(filterParam.FamilyName))
+                            if (filterParam.FamilyName.Contains("*") & !(famName.Contains(filterParam.FamilyName.Replace("*", ""))))
+                                continue;
+                            else if (!filterParam.FamilyName.Contains("*"))
+                                continue;
+                        bool isValid = true;
+                        foreach (var p in filterParam.parameters)
+                        {
+                            if (p.Value.Equals("")) continue;
+                            Parameter par = elementCategory.LookupParameter(p.Name);
+                            if (par == null)
+                                par = doc.GetElement(elementCategory.GetTypeId()).LookupParameter(p.Name);
+                            if (par == null)
+                                continue;
+                            string parValue = fw.GetParameterValue(par);
+                            if (p.Value.Contains("*") & parValue.Contains(p.Value.Replace("*", "")))
+                                continue;
+                            if (parValue == p.Value)
+                                continue;
+                            isValid = false;
+                            break;
+
+                        }
+                        if (!isValid)
+                        {
+                            //Не произошло совпадение
+                            continue;
+                        }
+                        Parameter paramToSet = elementCategory.LookupParameter(filterParam.ParameterToSet);
+                        if (paramToSet == null)
+                        {
+                            if (fs != null)
+                                paramToSet = fs.LookupParameter(filterParam.ParameterToSet);
+                            if (paramToSet == null)
+                            {
+                                errorMessages.Add(new ErrorMessage(string.Format("Лист Параметры. Отсутствует параметр \"{0}\"", filterParam.ParameterToSet), elementCategory.Id));
+                                continue; //Нет заполняемого параметра
+                            }
+                        }
+                        AnalyticalModelSurface sur = null;
+
+                        isSetParameter = true;
+                        if (paramToSet.IsReadOnly)
+                        {
+                            errorMessages.Add(new ErrorMessage(string.Format("Лист Параметры. Значение параметра \"{0}\" задано формулой в семействе. ", filterParam.ParameterToSet), elementCategory.Id));
+                            continue;
+                        }
+                        paramToSet.Set(filterParam.ParameterValue);
+                        break;
+                    }
+                    if (!isSetParameter)
+                    {
+                        //Элемент не найден в Excel
+                    }
+                }
+
+            }
         }
 
         private void GetParameterByFormula(List<VK_Element> vkOverlap, Element element)
@@ -435,8 +472,6 @@ namespace Revit_Spec_VK
                     if (isCorner) value = Math.Round(parameter.AsDouble() * 180 / 3.14, 1).ToString();
                     else
                         value = Math.Round((parameter.AsDouble() * 304.8), 1).ToString(); break;
-
-                    break;
                 case StorageType.Integer: value = Math.Round((parameter.AsInteger() * 304.8), 0).ToString(); break;
                 case StorageType.String: value = Convert.ToString(parameter.AsString()); break;
             }
